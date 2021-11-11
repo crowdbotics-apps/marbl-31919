616db6f6eb5f193108a38e0ed18bc136ac76eca0
Crowdbotics <team@crowdbotics.com>
2021-11-11 06:57:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "marbl_31919",
+  url: "https://marbl_31919.botics.co",
+  api: "https://marbl_31919.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
